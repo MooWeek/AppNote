533fe1495053e8d796eda579f98244b53b2f5811
Andrii Andrunevchyn <andrii.andrunevchyn@gmail.com>
2021-08-06 03:52:01 +0300

    Update

--- Services/NoteService.cs
+++ Services/NoteService.cs
@@ -8,20 +8,43 @@ namespace AppNote.Services
     {
         protected readonly AppNoteDbContext _dbContext;
 
+        public List<NoteClass> FindedNotes { get; private set; }
+
         public NoteService(AppNoteDbContext dbContext)
         {
             _dbContext = dbContext;
+            FindedNotes = _dbContext.Notes.ToList();
         }
 
-        public List<NoteClass> DisplayNotes()
+        public IList<NoteClass> DisplayNotes()
         {
-            return _dbContext.Notes.ToList();
+            return FindedNotes;
+        }
+
+        public void FindNotes(string searchStr)
+        {
+            if (string.IsNullOrWhiteSpace(searchStr))
+                FindedNotes = _dbContext.Notes.ToList();
+            else
+            {
+                IList<NoteClass> notes = _dbContext.Notes.ToList();
+
+                //Find matching in title
+                IList<NoteClass> matchingNotes = notes.Where(item => item.Title.ToLower().Contains(searchStr.ToLower())).ToList();
+                //Find matching in text
+                FindedNotes =  matchingNotes.Concat(notes.Where(item => item.Text.ToLower().Contains(searchStr.ToLower()))).ToList();
+            }
         }
 
         public void AddNewNote(NoteClass newNote)
         {
             _dbContext.Notes.Add(newNote);
             _dbContext.SaveChanges(); 
+        }
+
+        public void UpdateNote()
+        {
+            _dbContext.SaveChanges();
         }
     }
 }
